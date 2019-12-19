0fe30a8d0775aa262424ac6a349dccbe86450251
Pocky Yang <31677343+pockyyang@users.noreply.github.com>
2019-12-19 21:50:16 +0800

    Add individual files for each video page

--- BlibiliDownloader.py
+++ BlibiliDownloader.py
@@ -5,6 +5,7 @@ import sys
 from selenium import webdriver
 from you_get import common as you_get
 import time
+import os
 # Get the number of video list pages and uper's name
 def Get_VodPagesNum(uper_Uid_num):
     uper_Uid_str = str(uper_Uid_num)
@@ -12,6 +13,8 @@ def Get_VodPagesNum(uper_Uid_num):
     firstpage = 'https://space.bilibili.com/' + uper_Uid_str + '/video?tid=0&page=0'
     # Open Firefox
     browser = webdriver.Firefox()
+    option = webdriver.FirefoxOptions()
+    option.add_argument('-headless')
     # open uper's video list first page
     browser.get(firstpage)
     # Wait 2s
@@ -20,7 +23,7 @@ def Get_VodPagesNum(uper_Uid_num):
     pager_total_text = browser.find_element_by_class_name('be-pager-total').text
     # Get the name of Uper
     uper_name = browser.find_element_by_id('h-name').text
-    print("Uper's name：",uper_name)
+    print("Uper's name：", uper_name)
     print(pager_total_text)
     pager_total_str = ''
     # Transfer the page number from string to num
@@ -33,8 +36,7 @@ def Get_VodPagesNum(uper_Uid_num):
         pager_total_num = int(pager_total_str)
     print('video list total number：', pager_total_num)
     browser.close()
-    return [pager_total_num,uper_name]
-
+    return [pager_total_num, uper_name]
 
 
 # Define the download function
@@ -42,11 +44,11 @@ def downloadUperVod(uper_Uid_num, pager_total_num, directory):
     # transfer the uper's Uid into string
     uper_Uid_str = str(uper_Uid_num)
     # In  order to cover the last video list page in following for loop
-    pager_total_num = pager_total_num+1
-    for page_index in range(2,pager_total_num):
-        page_index_num= page_index
+    pager_total_num = pager_total_num + 1
+    for page_index in range(1, pager_total_num):
+        page_index_num = page_index
         page_index_str = str(page_index_num)
-        page_url = 'https://space.bilibili.com/'+uper_Uid_str+'/video?tid=0&page='+page_index_str
+        page_url = 'https://space.bilibili.com/' + uper_Uid_str + '/video?tid=0&page=' + page_index_str
         print(page_url)
         # Open FireFox
         browser = webdriver.Firefox()
@@ -58,19 +60,23 @@ def downloadUperVod(uper_Uid_num, pager_total_num, directory):
         # Counter of the downloaded video in this page
         count = 1
         # Create a file path for each video list page,all videos in this page will be downloaded in individual file
-        directory = directory+'/page'+page_index_str
+        Downlaod_directory = directory + '/page'+page_index_str
+        isExists= os.path.exists(Downlaod_directory)
+        if not isExists:
+         os.mkdir(Downlaod_directory)
         for url in vod_urlList:
-            print('Downloading the ',page_index,'th page ',count,'th video')
-            count = count+1
+            print('Downloading the ', page_index, 'th page ', count, 'th video')
+            count = count + 1
             href = url.get_attribute("href")
-            sys.argv = ['you-get', '-o', directory, href]
+            print(href)
+            # sys.argv = ['you-get', '-o', Downlaod_directory, href, '--debug']
+            sys.argv = ['you-get', '-o', Downlaod_directory, href]
             you_get.main()
         browser.close()
-
 # Set the file path to store videos
-filepath ='/filepath/'
-#Set Uper's Uid
-uper_uid = xxxxxxxxx
-[pager_total_num, uper_name]=Get_VodPagesNum(uper_uid)
-directory =filepath+uper_name
-downloadUperVod(uper_uid,pager_total_num,directory)
+filepath = 'H:/软件脚本/python/B站视频批量下载/source_B/'
+# Set Uper's Uid
+uper_uid = 16539048
+[pager_total_num, uper_name] = Get_VodPagesNum(uper_uid)
+directory = filepath + uper_name
+downloadUperVod(uper_uid, pager_total_num, directory)
